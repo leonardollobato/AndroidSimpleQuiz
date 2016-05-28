41707c41e74e875e233751c49bab771d36e6b85e
Leonardo Lobato <leonardo.lobato@gmail.com>
2016-05-28 00:11:32 -0300

    Finished

--- app/src/main/java/com/example/leonardollobato/geoquiz/CheatActivity.java
+++ app/src/main/java/com/example/leonardollobato/geoquiz/CheatActivity.java
@@ -1,57 +1,116 @@
 package com.example.leonardollobato.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class CheatActivity extends AppCompatActivity {
 
+    private static final String TAG = "CHEATE-ACTIVITY-";
     private static final String EXTRA_ANSWER_IS_TRUE =
             "com.example.leonardollobato.geoquiz.answer_is_true";
     private static final String EXTRA_ANSWER_IS_SHOWN =
             "com.example.leonardollobato.geoquiz.answer_shown";
 
+
+
     private boolean mAnswerIsTrue;
     private TextView mAnswerTextView;
     private Button mShowAnswer;
+    private int mAnswerStringId;
 
+    /**
+     * Customized Intent, specific for pass data from parent Activity to this one (CheatActivity).
+     * @param packageContext
+     * @param answerIsTrue
+     * @return
+     */
     public static Intent newIntent(Context packageContext, boolean answerIsTrue){
         Intent i = new Intent(packageContext, CheatActivity.class);
         i.putExtra(EXTRA_ANSWER_IS_TRUE,answerIsTrue);
         return i;
     }
 
+    /**
+     * Returns true if the answer was cheated or false if it was not.
+     * @param result
+     * @return
+     */
     public static boolean wasAnswerShow(Intent result){
         return result.getBooleanExtra(EXTRA_ANSWER_IS_SHOWN, false);
     }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
 
-        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE,false);
+
+
+        // Get the value passed from the Quiz Activity about the right answer
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
 
         mAnswerTextView = (TextView) findViewById(R.id.answerTextView);
         mShowAnswer = (Button) findViewById(R.id.showAnswerButton);
+
+
         mShowAnswer.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+
+                // check if the answer received from the QuizActivity is True or False and Set the View
                 if(mAnswerIsTrue){
-                    mAnswerTextView.setText(R.string.true_button);
+                    mAnswerStringId = R.string.true_button;
                 }else{
-                    mAnswerTextView.setText(R.string.false_button);
+                    mAnswerStringId = R.string.false_button;
                 }
+
+                mAnswerTextView.setText(mAnswerStringId);
                 setAnswerShowResult(true);
+
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+                    int cx = mShowAnswer.getWidth() / 2;
+                    int cy = mShowAnswer.getHeight() / 2;
+                    float radius = mShowAnswer.getWidth();
+
+                    Animator anim = ViewAnimationUtils
+                            .createCircularReveal(mShowAnswer, cx, cy, radius, 0);
+
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mAnswerTextView.setVisibility(View.VISIBLE);
+                            mShowAnswer.setVisibility(View.INVISIBLE);
+                        } });
+                    anim.start();
+                }else{
+                    mAnswerTextView.setVisibility(View.VISIBLE);
+                    mShowAnswer.setVisibility(View.INVISIBLE);
+                }
+
             }
         });
 
-
     }
 
+
+
+    /**
+     * Create a Intent that has the flag that indicates if the user cheated or not
+     * then it is sent to the parent activity by the setResult method that receive a general status and the intent with the data
+     * @param isAnswerShown
+     */
     private void setAnswerShowResult(boolean isAnswerShown){
         Intent data = new Intent();
         data.putExtra(EXTRA_ANSWER_IS_SHOWN, isAnswerShown);
--- app/src/main/java/com/example/leonardollobato/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/leonardollobato/geoquiz/QuizActivity.java
@@ -2,10 +2,8 @@ package com.example.leonardollobato.geoquiz;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -18,15 +16,6 @@ public class QuizActivity extends AppCompatActivity {
     private static final String TAG = "QuizActivity";
     private static final String KEY_INDEX = "index";
     private static final int REQUEST_CODE_CHEAT = 0;
-
-    private Button mTrueButton;
-    private Button mFalseButton;
-    private View mNextButton;
-    private View mPreviousButton;
-    private Button mCheatButton;
-    private TextView mQuestionTextView;
-    private boolean mIsCheater;
-
     private final Question[] mQuestionBank = new Question[]{
             new Question(R.string.question_oceans, true),
             new Question(R.string.question_mideast, false),
@@ -34,7 +23,13 @@ public class QuizActivity extends AppCompatActivity {
             new Question(R.string.question_americas, true),
             new Question(R.string.question_asia, true)
     };
-
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private View mNextButton;
+    private View mPreviousButton;
+    private Button mCheatButton;
+    private TextView mQuestionTextView;
+    private boolean mIsCheater;
     private int mCurrentIndex = 0;
 
     private void updateQuestion() {
@@ -72,10 +67,6 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-        Log.d(TAG, "onCreate(Bundle) called");
-
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
@@ -146,18 +137,15 @@ public class QuizActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode != Activity.RESULT_OK) {
+        if (resultCode != Activity.RESULT_OK)
             return;
-        }
 
-        if (resultCode == REQUEST_CODE_CHEAT) {
-            if (data == null) {
+        if (requestCode == REQUEST_CODE_CHEAT) {
+            if (data == null)
                 return;
-            }
 
             mIsCheater = CheatActivity.wasAnswerShow(data);
         }
-        //super.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
@@ -167,33 +155,4 @@ public class QuizActivity extends AppCompatActivity {
         outState.putInt(KEY_INDEX, mCurrentIndex);
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        Log.d(TAG, "onStart() called");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "onPause() called");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG, "onResume() called");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG, "onStop() called");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "onDestroy() called");
-    }
 }
